73c9387f36d4e02f97c0b31ce6f7e1234eed48e2
maximeC <maxime.cantie@hotmail.fr>
2021-10-07 17:09:08 +0200

    GUI autolog

---
+++ AutoLog/GUI.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+from PyQt5 import *
+from PyQt5 import QtWidgets
+from PyQt5.QtCore import QDateTime, Qt, QTimer
+from PyQt5.QtWidgets import *
+
+import sys
+
+class WidgetGallery(QDialog):
+    def __init__(self, parent=None):
+        super(WidgetGallery, self).__init__(parent)
+        self.resize(1200,800)
+        self.setWindowTitle("AutoLog")
+        
+        combobox = QComboBox(self)
+        combobox.addItem('Syslog')
+        combobox.addItem('Apache')
+        combobox.addItem('User')
+
+        styleLabel = QLabel("&Type de log:")
+        styleLabel.setBuddy(combobox)
+
+        self.createBottomLeftTabWidget()
+
+        topLayout = QHBoxLayout()
+        topLayout.addWidget(styleLabel)
+        topLayout.addWidget(combobox)
+        topLayout.addStretch(1)
+
+        mainLayout = QGridLayout()
+        mainLayout.addLayout(topLayout, 0, 0, 1, 1)
+        mainLayout.addWidget(self.bottomLeftTabWidget, 1, 0)
+
+        self.setLayout(mainLayout)
+
+        
+    def createBottomLeftTabWidget(self):
+        self.bottomLeftTabWidget = QTabWidget()
+        self.bottomLeftTabWidget.setSizePolicy(QSizePolicy.Preferred,
+                QSizePolicy.Ignored)
+
+        tab1 = QWidget()
+        tableWidget = QTableWidget(10,10)
+
+        tab1hbox = QHBoxLayout()
+        tab1hbox.setContentsMargins(5, 5, 5, 5)
+        tab1hbox.addWidget(tableWidget)
+        tab1.setLayout(tab1hbox)
+
+        
+        self.bottomLeftTabWidget.addTab(tab1, "&Table")
+
+
+
+if __name__ == '__main__':
+    app= QtWidgets.QApplication(sys.argv)
+    app.setStyle('Fusion')
+
+    data = [
+          [4, 9, 2, 0],
+          [1, 0, 0, 0],
+          [3, 5, 0, 0],
+          [3, 3, 2, 0],
+          [7, 8, 9, 0]
+        ]
+    
+    gallery = WidgetGallery()
+    gallery.show()
+
+    app.exec_()
