c7c16278ce4f14eb66afd67ac121d67a5083a807
gplata <georges.plata@tuta.io>
2021-10-07 17:12:27 +0200

    update kernLog and main

--- AutoLog/main.py
+++ AutoLog/main.py
@@ -1,7 +1,13 @@
 from classApacheLog import apacheLog
+from classKernLog import kernLog
 
 
 print("TEST=============CLASS APACHE")
 print("TEST=apachelog.testlog()")
 print(apacheLog("AutoLog/logFiles/apache.log").testLog())
 print(apacheLog("AutoLog/logFiles/apache.log").parseLog())
+
+print("TEST=============CLASS KERNEL")
+print("TEST=kernlog.testlog()")
+print(kernLog("AutoLog\logFiles\kern.log").testLog())
+print(kernLog("AutoLog/logFiles/kern.log").getContent())
