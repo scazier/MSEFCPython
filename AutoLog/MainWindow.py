bdb19664b8ca577c7a9c717e2d26be5e06dd37aa
m0xy <m0xy@NP50DE>
2021-12-25 23:35:07 +0100

    Add web data project and export of logs

--- AutoLog/MainWindow.py
+++ AutoLog/MainWindow.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 import os
+import re
 import sys
+import requests
 import exiftool
 from datetime import time
 from pathlib import Path
+from bs4 import BeautifulSoup
 
 import pandas as pd
 from PyQt5 import QtCore, QtWidgets
@@ -15,7 +18,7 @@ from PyQt5.QtWidgets import (QAction, QApplication, QComboBox, QFileDialog,
                              QMenu, QTableView, QTableWidget, QTextEdit,
                              QWidget, QTabWidget, QVBoxLayout, QHBoxLayout,
                              QListWidget, QListWidgetItem, QPushButton,
-                             QHeaderView)
+                             QHeaderView, QSizePolicy, QMessageBox)
 
 from main import logAnalysis
 # from numpyArrayModel import NumpyArrayModel
@@ -55,10 +58,11 @@ class TableWidget(QWidget):
 
         self.tabs.addTab(self.tab1,"Analog")
         self.tabs.addTab(self.tab2,"Metadata")
-        self.tabs.addTab(self.tab3,"Stat")
+        self.tabs.addTab(self.tab3,"URL Regex")
 
         self.analogUI()
         self.metadataUI()
+        self.websiteUI()
 
         self.layout.addWidget(self.tabs)
         self.setLayout(self.layout)
@@ -66,6 +70,7 @@ class TableWidget(QWidget):
     def analogUI(self):
         self.tab1.layout = QVBoxLayout()
         self.tab1.Hlayout = QHBoxLayout()
+        self.tab1.HBottomLayout = QHBoxLayout()
 
         self.table = QTableView()
 
@@ -74,6 +79,8 @@ class TableWidget(QWidget):
         self.comboBox = QComboBox(self.centralwidget)
         self.labelRegex = QLabel(self.centralwidget)
         self.labelNumLines = QLabel(self)
+        self.exportFilteredLogs = QPushButton("Export")
+        self.exportFilteredLogs.clicked.connect(self.exportLogs)
 
 
         self.gridLayout = QGridLayout(self.centralwidget)
@@ -98,6 +105,9 @@ class TableWidget(QWidget):
         self.tab1.layout.addLayout(self.tab1.Hlayout)
         self.tab1.layout.addWidget(self.table)
         self.tab1.layout.addWidget(self.labelNumLines)
+        self.tab1.HBottomLayout.addWidget(self.labelNumLines, alignment=QtCore.Qt.AlignLeft | QtCore.Qt.AlignBottom)
+        self.tab1.HBottomLayout.addWidget(self.exportFilteredLogs, alignment=QtCore.Qt.AlignRight | QtCore.Qt.AlignBottom)
+        self.tab1.layout.addLayout(self.tab1.HBottomLayout)
 
         self.tab1.setLayout(self.tab1.layout)
 
@@ -128,6 +138,112 @@ class TableWidget(QWidget):
         self.tab2.layout.addLayout(self.tab2.HBottomLayout)
         self.tab2.setLayout(self.tab2.layout)
 
+    def websiteUI(self):
+        self.tab3.layout = QVBoxLayout()
+        self.tab3.HFirstLayout = QHBoxLayout()
+        self.tab3.HSecondayout = QHBoxLayout()
+        self.tab3.HMainLayout = QHBoxLayout()
+        self.tab3.VSubmainLayout = QVBoxLayout()
+        self.tab3.HBottomLayout = QHBoxLayout()
+
+        self.labelURL = QLabel("URL:")
+        self.lineURL = QLineEdit()
+        self.labelURLRegex = QLabel("Regex:")
+        self.lineRegex = QComboBox()
+        self.lineRegex.edit = QLineEdit()
+        self.lineRegex.setLineEdit(self.lineRegex.edit)
+        self.lineRegex.setSizePolicy(QSizePolicy.Expanding,QSizePolicy.Preferred)
+        self.resultRegex = QListWidget()
+        self.browseRegex = QPushButton("",self)
+        self.browseRegex.setIcon(QIcon("../imgs/browsing.png"))
+        self.browseRegex.clicked.connect(self.browseURL)
+        self.exportRegex = QPushButton("Export")
+        self.exportRegex.clicked.connect(self.exportMatchedRegex)
+        self.regexMatches = QLabel()
+
+        self.loadRegex()
+
+        self.tab3.HFirstLayout.addWidget(self.labelURL)
+        self.tab3.HFirstLayout.addWidget(self.lineURL)
+        self.tab3.HSecondayout.addWidget(self.labelURLRegex)
+        self.tab3.HSecondayout.addWidget(self.lineRegex)
+        self.tab3.HBottomLayout.addWidget(self.regexMatches, alignment=QtCore.Qt.AlignLeft | QtCore.Qt.AlignBottom)
+        self.tab3.HBottomLayout.addWidget(self.exportRegex, alignment=QtCore.Qt.AlignRight | QtCore.Qt.AlignBottom)
+
+        self.tab3.VSubmainLayout.addLayout(self.tab3.HFirstLayout)
+        self.tab3.VSubmainLayout.addLayout(self.tab3.HSecondayout)
+        self.tab3.HMainLayout.addLayout(self.tab3.VSubmainLayout)
+        self.tab3.HMainLayout.addWidget(self.browseRegex)
+        self.tab3.layout.addLayout(self.tab3.HMainLayout)
+        self.tab3.layout.addWidget(self.resultRegex)
+        self.tab3.layout.addLayout(self.tab3.HBottomLayout)
+        self.tab3.setLayout(self.tab3.layout)
+
+    def loadRegex(self):
+        with open("URLRegex.conf") as regexConf:
+            for line in regexConf:
+                self.lineRegex.addItem(line.split(';')[0])
+
+    def browseURL(self):
+        if self.lineURL.text() == "":
+            msg = QMessageBox()
+            msg.setWindowTitle("Error")
+            msg.setInformativeText("No URL given")
+            msg.setIcon(QMessageBox.Warning)
+            msg.exec_()
+            return
+
+        # Load basic regex (IP,emails,phone, ...)
+        regex = ""
+        with open("URLRegex.conf") as regexConf:
+            for line in regexConf:
+                line = line.split(';')
+                if self.lineRegex.edit.text().lower() == line[0].lower():
+                    regex = line[1]
+
+        if regex == "":
+            regex = self.lineRegex.edit.text()#46.175.70.69
+
+        self.resultRegex.clear()
+        response = requests.get(self.lineURL.text())
+        soup = BeautifulSoup(response.text,'html.parser')
+
+        if response.status_code != 200:
+            msg = QMessageBox()
+            msg.setWindowTitle("Error")
+            msg.setInformativeText("An error occured, the URL return a status code: %" % response.status_code)
+            msg.setIcon(QMessageBox.Warning)
+            msg.exec_()
+            return
+
+        result = soup.find_all(text=re.compile(regex.strip()))
+        for res in result:
+            if re.fullmatch(re.compile(regex.strip()),res) is not None:
+                QListWidgetItem(res,self.resultRegex)
+            else:
+                for _res in re.finditer(re.compile(regex.strip()),res):
+                    QListWidgetItem(res[_res.start():_res.end()],self.resultRegex)
+
+            self.regexMatches.setText("{} match".format(self.resultRegex.count()))
+
+    def exportMatchedRegex(self):
+        if self.resultRegex.count() == 0:
+            msg = QMessageBox()
+            msg.setWindowTitle("Error")
+            msg.setInformativeText("You cannot export an empty list")
+            msg.setIcon(QMessageBox.Warning)
+            msg.exec_()
+            return
+
+        filepath, _ = QFileDialog.getSaveFileName(self, "Export regex", "", "csv")
+        if filepath == "":
+            return
+
+        with open(filepath+'.csv','w') as file:
+            for index in range(self.resultRegex.count()):
+                file.write(self.resultRegex.item(index).text()+'\n')
+
+
     def setMetadataPath(self):
         path = QFileDialog.getExistingDirectory(self, 'Open file','/home/')
         if path != "":
@@ -138,6 +254,7 @@ class TableWidget(QWidget):
             tmpPath = self.linePath.text()[self.linePath.text().find("file://")+7:]
             self.linePath.clear()
             self.linePath.setText(tmpPath.strip())
+            print(tmpPath.strip().encode('utf-8'))
 
         self.listWidget.clear()
         for root,directory,files in os.walk(self.linePath.text()):
@@ -202,10 +319,33 @@ class TableWidget(QWidget):
     def on_lineEdit_textChanged(self, text):
         search = QRegExp(text, Qt.CaseInsensitive, QtCore.QRegExp.RegExp)
         self.proxy.setFilterRegExp(search)
+        self.labelNumLines.setText("%s lines" % self.table.model().rowCount())
 
     def on_comboBox_currentIndexChanged(self, index):
         self.proxy.setFilterKeyColumn(index)
+        self.labelNumLines.setText("%s lines" % self.table.model().rowCount())
 
+    def exportLogs(self):
+        model = self.table.model()
+
+        if model.rowCount() == 0:
+            msg = QMessageBox()
+            msg.setWindowTitle("Error")
+            msg.setInformativeText("You cannot export an empty list")
+            msg.setIcon(QMessageBox.Warning)
+            msg.exec_()
+            return
+
+        filepath, _ = QFileDialog.getSaveFileName(self, "Export regex", "", "csv")
+        if filepath == "":
+            return
+
+        with open(filepath+'.csv','w') as file:
+            for row in range(model.rowCount()):
+                line = []
+                for column in range(model.columnCount()):
+                    line.append(model.data(model.index(row,column)))
+                file.write(";".join(line)+'\n')
 
 def main():
     app = QApplication(sys.argv)
